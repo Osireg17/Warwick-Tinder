1fdc8b060085fac7e475f651b71f2b03a19be1ff
Osi Obomighie <93836683+Osireg17@users.noreply.github.com>
2024-11-15 09:00:13 +0000

    Add a blank line for improved code readability in CountdownTimer component

--- client/app/components/countdown-timer.tsx
+++ client/app/components/countdown-timer.tsx
@@ -6,6 +6,7 @@ interface CountdownTimerProps {
     timeLeft: TimeLeft;
 }
 
+
 export const CountdownTimer = ({ timeLeft }: CountdownTimerProps) => {
     return (
         <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
